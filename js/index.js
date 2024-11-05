94480c7fe571c0dc46088ba7e84ff808ccb1e47b
jochuzanatta <jzanatta@udesa.edu.ar>
2024-11-05 11:27:14 -0300

    cambio en el css para utilizar una misma clase para todos los elementos flex

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ fetch(URL)
     let recipes = ""
     for (let i = 0; i < 10; i++) {
         recipes += 
-        `<article class="artRec">
+        `<article class="artRec , elementosFlex">
             <img src= ${recetas[i].image} alt=''>
             <h2> ${recetas[i].name} </h2>
             <h4> Dificultad: ${recetas[i].difficulty} </h4>
@@ -27,7 +27,7 @@ fetch(URL)
     cargarMas.addEventListener("click", function () {
         for (let a = 10; a < 20; a++) {
             masRecetas +=
-        `<article class="artFormCargar">
+        `<article class="artFormCargar , elementosFlex">
             <img class="img" src= ${recetas[a].image} alt=''>
             <h2> ${recetas[a].name} </h2>
             <h4> Dificultad: ${recetas[a].difficulty} </h4>
