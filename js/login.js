6a62f7e060fe88244421826f53a363840552aa4f
alinechana <achana@udesa.edu.ar>
2024-11-08 11:01:43 -0300

    js de los forms

---
+++ js/login.js
@@ -0,0 +1,21 @@
+let email = document.querySelector(".email");
+
+let contra = document.querySelector("contra");
+
+let login = document.querySelector(".login");
+
+login.addEventListener("submit", function() {
+    if (contra.length() == 0): 
+        alert("Por favor complete el campo contraseña");
+
+    if (email).length() == 0): 
+        alert("Por favor complete el campo email");
+
+})
+    
+
+
+      
+
+
+
