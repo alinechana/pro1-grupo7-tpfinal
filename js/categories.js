3f22b270319942281da20e20aa011733de005113
JustinaNavarro <justinanavarro@udesa.edu.ar>
2024-11-15 12:08:45 -0300

    js de categories

--- js/categories.js
+++ js/categories.js
@@ -1,20 +1,30 @@
-const URL = "https://dummyjson.com/recipes/tags"
+let searchTags = document.querySelector(".searchTags")
 
+let qs = location.search;
+let qsObj = new URLSearchParams(qs)
+let tag = qsObj.get("tag")
+let URL = `https://dummyjson.com/recipes/tag/${tag}`
 
 fetch(URL)
 .then(function (response) {
+    
     return response.json();
 })
 .then(function (data) {
+    let agregarTags = ""
+    let recipes = data.recipes
 
-   
-
-
-
-
-
+    for (let i = 0; i < recipes.length; i++) {
+        agregarTags += 
+        `<article>
+            <img src= ${recipes[i].image} alt=''>
+            <h2> ${recipes[i].name} </h2>
+        </article>
+        `
+    }
+    searchTags.innerHTML += agregarTags
 })
-.catch(function (error) {
+/*.catch(function (error) {
     return console.log(error);
     ;
-}) 
+})*/ 
