9958cc608a000d82f691c29ec526370a7ac8cfa1
JustinaNavarro <justinanavarro@udesa.edu.ar>
2024-11-15 12:54:39 -0300

    cambios en js de category

---
+++ js/categories.js
@@ -0,0 +1,26 @@
+let qs = location.search;
+let qsObj = new URLSearchParams(qs)
+let tag = qsObj.get("tag")
+
+let todasCategorias = document.querySelector(".todasCategorias")
+let url = 'https://dummyjson.com/recipes/tags';
+console.log(url);
+
+fetch(url)
+.then(function (response) {
+    return response.json()
+})
+.then(function (data) {
+    let listaTags = ""
+    for (let i = 0; i < data.length; i++) {
+        let tag = data[i]
+        listaTags +=
+        `<h3><a href="./category.html?tag=${tag}">${tag}</a></h3>`
+    }
+    todasCategorias.innerHTML = listaTags
+
+})
+.catch(function (erorr) {
+    return console.log(error);
+    
+})
