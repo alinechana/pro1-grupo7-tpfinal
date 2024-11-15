ecb1cdc0d0dbef038dbc3702191642dfa52f9b95
alinechana <achana@udesa.edu.ar>
2024-11-15 18:38:54 -0300

    js de results

---
+++ js/search-results.js
@@ -0,0 +1,38 @@
+let searchResults = document.querySelector(".search-results")
+
+let qs = location.search;
+let qsObj = new URLSearchParams(qs)
+let value = qsObj.get("q")
+
+const URL = 'https://dummyjson.com/recipes/search?q=${value}'
+
+let buscador = document.querySelector(".buscador")
+
+fetch(URL)
+.then(function (response) {
+    return response.json();
+    
+})
+.then(function (data) {
+    let recetas = " "
+    let resultados = data.recipes
+    for (let i = 0; i < resultados.length; i++) {
+        recetas += 1 
+
+        `<article>
+            <img src= ${recipes[i].image} alt=''>
+            <p> Receta: ${recipes[i].name}</p>
+            <p> Link: ${recipes[i].instructions} </p>
+        </article>`
+        
+    }
+    searchResults.innerHTML = recetas
+})
+.catch( function (error){
+    return console.log(error);
+})
+
+
+
+
+
