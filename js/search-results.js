d7c8f0656218a0433bc6f9004e1363e148c399b9
JustinaNavarro <justinanavarro@udesa.edu.ar>
2024-11-22 10:31:16 -0300

    css

--- js/search-results.js
+++ js/search-results.js
@@ -1,4 +1,4 @@
-let searchResults = document.querySelector(".search-results")
+let searchResults = document.querySelector(".searchResults")
 
 let qs = location.search;
 let qsObj = new URLSearchParams(qs)
@@ -20,7 +20,7 @@ fetch(URL)
     let resultados = data.recipes
     for (let i = 0; i < resultados.length; i++) {
         recetas += 
-        `<article>
+        `<article class="articleSearch">
             <img src= ${resultados[i].image} alt=''>
             <p> Receta: ${resultados[i].name}</p>
             <p class="recetasData"><a href="./receta.html?id=${resultados[i].id}">Presione para más detalles</a></p>
